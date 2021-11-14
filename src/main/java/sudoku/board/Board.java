7bf26ec10c3ea1b0b5757e1921deacd9aab3066e
Martin Berg Alstad <89257606+h600878@users.noreply.github.com>
2021-11-14 23:28:22 +0100

    2 dimensional array

--- src/main/java/sudoku/board/Board.java
+++ src/main/java/sudoku/board/Board.java
@@ -2,14 +2,16 @@ package sudoku.board;
 
 public class Board {
 
-    private Block[] board;
+    private Block[][] board;
 
     public Board() {
-        board = new Block[9];
+        board = new Block[3][3];
 
         //Creates i objects of 3x3 Squares
-        for (int i = 0; i < board.length; i++) {
-            board[i] = new Block();
+        for (int r = 0; r < board.length; r++) {
+            for (int k = 0; k < board[r].length; k++) {
+                board[r][k] = new Block();            
+            }
         }
     }
 }
