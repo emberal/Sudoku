51b0343a34a17eda622272ef3269ecdc1c57b8cb
Martin Berg Alstad <89257606+h600878@users.noreply.github.com>
2021-11-14 23:25:53 +0100

    Update Square.java

--- src/main/java/sudoku/board/Square.java
+++ src/main/java/sudoku/board/Square.java
@@ -3,6 +3,7 @@ package sudoku.board;
 public class Square {
 
     private byte nr;
+    private boolean editable; //Numbers set by the game, should not be editable by the player
 
     public Square() {
         //TODO place random numbers on the board, find out how many...
