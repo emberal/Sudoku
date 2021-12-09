a8e65025deda20cddc6b0552b3157cd1ae5d9350
Martin Berg Alstad <600878@stud.hvl.no>
2021-12-09 15:45:47 +0100

    completeHorizontal og completeVertical

--- src/main/java/sudoku/board/Board.java
+++ src/main/java/sudoku/board/Board.java
@@ -2,6 +2,8 @@ package sudoku.board;
 
 import sudoku.gui.Interface;
 
+import java.util.Arrays;
+
 public class Board {
 
     public static final int LEN = 3;
@@ -48,7 +50,23 @@ public class Board {
      * @return boolean - Returns true if all the numbers in a horizontal line are unique
      */
 
-    public boolean completeHorizontal(int n) { //TODO
+    public boolean completeHorizontal(int r) { //TODO Test
+
+        for (int i = 0; i < board[r].length; i++) {
+            if (board[r][i].completeHorizontal(r) != null) {
+                for (int j = 0; j < board[r].length; j++) {
+                    Square[] h1 = board[r][j].completeHorizontal(r);
+                    Square[] h2 = board[r][j+1].completeHorizontal(r);
+                    for (int h = 0; h < h1.length; h++) {
+                        for (int k = 0; k < h1.length; k++) {
+                            if (h1[h] != h2[k]) { //FIXME
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
